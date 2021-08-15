5a896b3e2131f238bf9e1d3afec7f0fd206247df
PopckovS <popckov5@yandex.ru>
2021-08-15 22:31:29 +0300

    Добавил в метод простой итерации интерактивный режим работы и режим для терминала.

--- __init__.py
+++ __init__.py
@@ -1,16 +1,11 @@
-from .root import half_division, simple_iteration
-from .differential import euler, runge_kutt
-from .integral import left_rectangles, simpson
+from .root.simple_iteration import simple_iteration, interactive_simple_iteration
+# from .differential import euler, runge_kutt
+# from .integral import left_rectangles, simpson
 
 # Все модули для импортирования пакета
 __all__ = [
     "NAME",
-    "half_division",
-    "simple_iteration",
-    "euler",
-    "runge_kutt",
-    "simpson",
-    "left_rectangles"
+    "simple_iteration", "interactive_simple_iteration",
     ]
 
 # Название пакета
--- root/simple_iteration.py
+++ root/simple_iteration.py
@@ -1,37 +1,26 @@
 """
-Лабораторная работа №1.1
 Используем метод простой итерации для нахождения корней уравнения.
-
-Пример уравнения:
-           f(x) = 2 cos x -7x = 0; при  = 0.01
-           f(x) = 7x = 2 cos x
-           f(x) = x = (2 cos x) / 7
-    Пусть x0 = 1 тогда:
-    x1 = (2 cos x0) / 7
-    x2 = (2 cos x1) / 7
-    x3 = (2 cos x2) / 7
-И так далее ... до тех пор пока |Xn+1 - Xn| <= E
 """
-import math
 
 
-def func(x):
-    """Функция для вычислений"""
-    return (2 * math.cos(x)) / 7
-
-
-def check_e(x_list, result, step, E):
+# Для работы в терминале
+def interactive_check_e(x_list, result, step, E):
     """Проверка рузультата на достижение точности E(кси)"""
     if abs(result - x_list[step]) < E:
         print('Точность перешла порог E (кси)')
         print('По формуле: |Xn+1 - Xn|')
-        print('|{next} - {prev}| = {result} <= {E}'.
-            format(next=result, prev=x_list[step], result=abs(result - x_list[step - 1]), E=E)
+        print('|{next} - {prev}| = {result} <= {E}'.format(
+                    next=result,
+                    prev=x_list[step],
+                    result=abs(result - x_list[step]),
+                    E=E
+                )
         )
         return True
 
 
-def show_step(step, current_x, result):
+# Для работы в терминале
+def interactive_show_step(step, current_x, result):
     """Показать информацию о текущем шаге"""
     print(f'Шаг №{step} при X{step} = {current_x} По формуле '
           f'(2 * math.cos({current_x})) / 7 = {result}'.
@@ -39,24 +28,8 @@ def show_step(step, current_x, result):
           )
 
 
-def start():
-    """
-    Цикл вычислений и проверок
-    """
-    x0, E, x_list, max_step = get_param()
-
-    for step in range(max_step):
-        current_x = x_list[step]
-        result = func(current_x)
-        show_step(step, current_x, result)
-        x_list.append(result)
-        if len(x_list) > 1 and check_e(x_list, result=result, step=step, E=E):
-            break
-    else:
-        print('Метод простой итерации не сошелся')
-
-
-def get_param():
+# Для работы в терминале
+def interactive_get_param():
     # Получаем данные от пользователя
     x0 = float(input('Введите X0 : '))
     E = float(input('Введите точность E(кси): '))
@@ -65,7 +38,48 @@ def get_param():
     return x0, E, x_list, max_step
 
 
+# Для работы в терминале
+def interactive_simple_iteration(function):
+    """
+    Цикл вычислений и проверок
+    """
+    x0, E, x_list, max_step = interactive_get_param()
+
+    for step in range(max_step):
+        current_x = x_list[step]
+        result = function(current_x)
+        interactive_show_step(step, current_x, result)
+        x_list.append(result)
+        if len(x_list) > 1 and interactive_check_e(x_list, result=result, step=step, E=E):
+            break
+    else:
+        print('Метод простой итерации не сошелся')
+
+
+# Не для интерактивной работы
+def simple_iteration(x0, E, function, max_step=100):
+    """
+    Цикл вычислений и проверок
+    """
+
+    x_list = [x0]
+
+    for step in range(max_step):
+        x = x_list[step]
+        # result = eval(function_expression)
+        result = function(x)
+        x_list.append(result)
+        if len(x_list) > 1 and abs(result - x_list[step]) < E:
+            return {'msg': 'OK',
+                    'Результат': result - x_list[step],
+                    'Все шаги': x_list,
+                    'E(кси)': E,
+                    'Количество шагов': step + 1
+                    }
+    else:
+        return {'msg': 'Метод простой итерации не сошелся'}
+
+
 # Запуск программы
 if __name__ == "__main__":
-    start()
-
+    interactive_simple_iteration()
